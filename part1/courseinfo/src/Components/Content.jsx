89bbf9b95dc4295f887b1c24380e06ce6b27e0ab
c-panagopoulos <216336981+c-panagopoulos@users.noreply.github.com>
2026-06-02 22:18:10 +0300

    Add part1 courseinfo exercises 1.1-1.2

---
+++ part1/courseinfo/src/Components/Content.jsx
@@ -0,0 +1,11 @@
+import Part from "./Part";
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part} exercises={props.exercises} />
+    </div>
+  );
+};
+
+export default Content;
